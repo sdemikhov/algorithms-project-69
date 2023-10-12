8f90f8e0a102703211cd8c149694a7051152feab
Sergey Demikhov <demikhov.sergey@gmail.com>
2023-10-13 00:10:17 +0400

    added search() feature, added tests for it, added jest, eslint

---
+++ __test__/search.test.js
@@ -0,0 +1,17 @@
+import { test, expect } from '@jest/globals';
+import search from '../src/index';
+
+const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+const doc3 = { id: 'doc3', text: "I'm your shooter." };
+const doc4 = { id: 'doc4', text: 'What is ashoot?' };
+const docs = [doc1, doc2, doc3, doc4];
+
+test('should find docs', () => {
+  expect(search(docs, 'shoot')).toStrictEqual(['doc1', 'doc2']);
+});
+
+test('should handle empty collection and not existed search target', () => {
+  expect(search([], 'shoot')).toStrictEqual([]);
+  expect(search(docs, 'not-existed')).toStrictEqual([]);
+});
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-export default () => {};
+const search = (docs, target) =>
+  docs
+    .filter(({ text }) => {
+      // try to escapes user input to prevent regex syntax error
+      const sanitized = target.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+      const m = text.match(new RegExp(`\\b(${sanitized})\\b`));
+      if (m == null) {
+        return false;
+      }
+      return true;
+    })
+    .map(({ id }) => id);
+
+export default search;
