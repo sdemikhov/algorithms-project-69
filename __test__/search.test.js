d67f247450ccddcdfef63386a2f2beaeaf0a223a
Sergey Demikhov <demikhov.sergey@gmail.com>
2023-10-15 21:44:12 +0400

    added simple fuzzy search

--- __test__/search.test.js
+++ __test__/search.test.js
@@ -7,13 +7,21 @@ const doc3 = { id: 'doc3', text: "I'm your shooter." };
 const doc4 = { id: 'doc4', text: 'What is ashoot?' };
 const docs = [doc1, doc2, doc3, doc4];
 
-test('should find docs', () => {
+test('should find single term in docs', () => {
   expect(search(docs, 'shoot')).toStrictEqual(['doc2', 'doc1']);
   expect(search(docs, 'pint')).toStrictEqual(['doc1']);
   expect(search(docs, 'pint!')).toStrictEqual(['doc1']);
 });
 
+test('should find multiple terms in docs', () => {
+  expect(search(docs, 'shoot at me')).toStrictEqual(['doc2', 'doc1']);
+  expect(search(docs, "I'm")).toStrictEqual(['doc3', 'doc1']);
+});
+
 test('should handle empty collection and not existed search target', () => {
   expect(search([], 'shoot')).toStrictEqual([]);
-  expect(search(docs, 'not-existed')).toStrictEqual([]);
+  expect(search(docs, 'notExisted')).toStrictEqual([]);
+  expect(search(docs, '')).toStrictEqual([]);
+  expect(search(docs, 'not existed')).toStrictEqual([]);
+  expect(search(docs, `}{|(/)[]!.`)).toStrictEqual([]);
 });
--- src/index.js
+++ src/index.js
@@ -1,19 +1,40 @@
-const search = (docs, target) => {
-  const termResult = target.match(/\w+/g);
-  if (termResult == null) {
-    return [];
-  }
-  const [term] = termResult;
-  return docs
-    .reduce((acc, doc) => {
-      const targetResult = doc.text.matchAll(new RegExp(`\\b(${term})\\b`, 'g'));
-      const match = [...targetResult];
-      if (match.length === 0) {
+const getMatchStats = (match) =>
+  match.reduce(
+    (acc, m) => {
+      const stored = acc.items[m];
+      if (stored) {
         return acc;
       }
-      return [...acc, { ...doc, matchLength: match.length }];
+      return {
+        items: { ...acc.items, [m]: true },
+        count: acc.count + 1,
+      };
+    },
+    { count: 0, items: {} },
+  );
+
+const search = (docs, target) => {
+  const terms = target.match(/\w+/g);
+  if (terms == null) {
+    return [];
+  }
+
+  return docs
+    .reduce((acc, doc) => {
+      const match = doc.text.match(new RegExp(`\\b(${terms.join('|')})\\b`, 'g'));
+      if (match == null) {
+        return acc;
+      }
+      const uniqueMatch = getMatchStats(match);
+      return [...acc, { ...doc, allMatchCount: match.length, uniqueMatchCount: uniqueMatch.count }];
     }, [])
-    .sort((doc1, doc2) => doc2.matchLength - doc1.matchLength)
+    .sort((doc1, doc2) => {
+      const uniqueMatchCountDiff = doc2.uniqueMatchCount - doc1.uniqueMatchCount;
+      if (uniqueMatchCountDiff === 0) {
+        return doc2.allMatchCount - doc1.allMatchCount;
+      }
+      return uniqueMatchCountDiff;
+    })
     .map(({ id }) => id);
 };
 
